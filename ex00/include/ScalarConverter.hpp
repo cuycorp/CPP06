778e1a4703806713bb0adf6f0110089f24579621
cuycorp <cuycorp@gmail.com>
2026-01-08 14:43:42 +0100

    setup

---
+++ ex00/include/ScalarConverter.hpp
@@ -0,0 +1,15 @@
+#ifndef SCALARCONVERTER_HPP
+# define SCALARCONVERTER_HPP
+# include <iostream>
+
+class ScalarConverter
+{
+    public:
+        ScalarConverter(void);
+        ScalarConverter(const ScalarConverter& other);
+        ScalarConverter &operator=(const ScalarConverter &other);
+        ~ScalarConverter();
+};
+
+#endif
+
